8d2bd1899e42d96338efc160b734f14d930cb4ed
hanan-bhat <bhat.hanan7@gmail.com>
2024-05-07 17:35:10 +0530

    test

--- db.js
+++ db.js
@@ -5,10 +5,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const MONGODB_URI = process.env.MONGODB_URI
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGODB_URI)
 mongoose.set('returnOriginal', false)
 const db = mongoose.connection
 
