eef9b34a7074ee6813292ed16da8a693dfa5312a
hanan-bhat <bhat.hanan7@gmail.com>
2024-05-07 17:32:51 +0530

    test

--- db.js
+++ db.js
@@ -8,9 +8,8 @@ const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 })
+mongoose.set('returnOriginal', false)
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
