073b3c3645143d1499fe9a7ea2154abf2d2c86d5
hanan-bhat <bhat.hanan7@gmail.com>
2024-05-07 17:28:21 +0530

    test

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 const MONGODB_URI = process.env.MONGODB_URI
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
